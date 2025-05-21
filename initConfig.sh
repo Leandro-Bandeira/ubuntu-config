ef8d7389bb258fdf7844e42440f6a99bc4849b29
Leandro Bandeira <leoviolinist22@gmail.com>
2025-05-20 22:31:41 -0300

    addings new commands

--- initConfig.sh
+++ initConfig.sh
@@ -10,3 +10,13 @@ sudo apt install helix
 
 # Move config helix
 mv helix_config/config.toml ~/.config/helix/config.toml
+
+# Install curl
+sudo apt install curl
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
+
+# Install vscode
+wget -O packages/vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+cd packages
+sudo apt install ./vscode.deb
+cd ..
